b53612072ddfe360c3fa3a279dabf9850546c545
Nii Darko Dodoo <niimajor4@gmail.com>
2023-04-08 18:11:20 +0000

    Vs Player all done

--- js/main.js
+++ js/main.js
@@ -4,14 +4,29 @@ const board = document.getElementById('play-area')
 // const restart = document.getElementById('restart')
 let infoDisplay = document.getElementById('info')
 const square = document.querySelector('.square-buttons')
-
+const notice = document.querySelector('.winning-notice')
+const container = document.getElementById('winning-container')
+const player1 = document.querySelector('.you')
+const player2 = document.querySelector('.cpu')
+const roundMark = document.getElementById('round-mark')
 
 const vsCPU = document.getElementById('vs-cpu')
 const vsPlayer = document.getElementById('vs-player')
 // infoDisplay = document.innerHTML = 'X turn'
 
 let currentPlayer = 'cross'
-let newPlayer = 'circle'
+let p1 = 'o'
+let p2 = 'x'
+player1.innerHTML = 'X (P2)'
+player2.innerHTML = 'O (P1)'
+let winCombo = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
+]
+let score = document.querySelector('.score')
+
+
 // Function to get Game board
 function getBoard(_cell, index) {
     for (let i = 0; i < cells.length; i++) {
@@ -27,6 +42,13 @@ function getVersus() {
     vsCPU.addEventListener('click', () => {
         document.getElementById('new-game-menu').style.display = 'none';
         document.getElementById('board').style.display = 'block'
+        toggleSelectionVsCPU()
+
+    })
+    vsPlayer.addEventListener('click', () => {
+        document.getElementById('new-game-menu').style.display = 'none';
+        document.getElementById('board').style.display = 'block'
+        toggleSelection()
 
     })
 }
@@ -46,8 +68,6 @@ function changeMark(e) {
         document.querySelector('#x').style.fill = 'var(--silver)'
         xButton.classList.remove('mark-style')
     })
-
-    // oButton.classList.remove('mark-style')
 
 }
 function addTick(e) {
@@ -74,14 +94,14 @@ function addTick(e) {
             e.target.appendChild(oMark)
             break;
     }
-    
+
     // if O mark is true, return X mark else return O mark and assign it to mark
     currentPlayer = currentPlayer === 'circle' ? 'cross' : 'circle';
     infoDisplay.innerHTML = `${currentPlayer} turn`
 
     // Remove event Listener on squares
     e.target.removeEventListener('click', addTick)
-    checkWin()
+    checkWin(p1, p2)
 }
 
 function setHoverState(mark) {
@@ -89,20 +109,12 @@ function setHoverState(mark) {
 
     square.classList.remove('cross');
 
-    if (mark == 'cross'){
+    if (mark == 'cross') {
         square.classList.add('circle')
     }
-    else{
+    else {
         square.classList.add('cross')
     }
-    // switch (mark) {
-    //     case 'cross':
-    //         square.classList.add(mark)
-    //         break;
-    //     case 'circle':
-    //         square.classList.add(mark)
-    //         break;
-    // }
 
 }
 
@@ -113,72 +125,131 @@ function reset() {
     })
 
 }
+function getPlayer() {
+    return toggleSelection()
 
+}
 
-// check winning combination
-const checkWin = () => {
-    let winCombo = [
-        [0, 1, 2], [3, 4, 5], [6, 7, 8],
-        [0, 3, 6], [1, 4, 7], [2, 5, 8],
-        [0, 4, 8], [2, 4, 6]
-    ]
+function crossWins() {
+    getPlayer()
+    console.log(p1)
     winCombo.forEach(combo => {
         const crossWins = combo.every(index =>
             cells[index].firstChild?.classList.contains('cross'))
-
         if (crossWins) {
-            console.log('cross Wins')
-            return
+            if (p1 == 'x') {
+                container.classList.add('show')
+                markImg = document.createElement('img')
+                markImg.src = './assets/icon-x.svg'
+                notice.innerText = 'PLAYER 1 WINS'
+                roundMark.appendChild(markImg)
+                console.log('x wins')
+                return
+            }
+            if (p2 == 'x') {
+                container.classList.add('show')
+                markImg = document.createElement('img')
+                markImg.src = './assets/icon-x.svg'
+                notice.innerText = 'PLAYER 2 WINS'
+                roundMark.appendChild(markImg)
+                console.log('x wins')
+            }
         }
-
     })
+}
+function circleWins() {
+    getPlayer()
+    console.log(p2)
     winCombo.forEach(combo => {
         const circleWins = combo.every(index =>
             cells[index].firstChild?.classList.contains('circle'))
         if (circleWins) {
-
-            console.log('circle Wins')
-            return
+            if (p1 == 'o') {
+                container.classList.add('show')
+                markImg = document.createElement('img')
+                markImg.src = './assets/icon-o.svg'
+                roundMark.appendChild(markImg)
+                notice.innerText = `Player 1 wins`
+                return
+            }
+            if (p2 == 'o') {
+                container.classList.add('show')
+                markImg = document.createElement('img')
+                markImg.src = './assets/icon-o.svg'
+                roundMark.appendChild(markImg)
+                notice.innerText = 'Player 2 WINS'
+                console.log('o wins')
+                return
+            }
         }
     })
-
+}
+// check winning combination
+const checkWin = () => {
+    crossWins()
+    circleWins()
     endGame(false)
 }
-// const toggleSelection = (e) => {
-//     const o = document.getElementById('O-mark')
-//     const x = document.getElementById('X-mark')
-//     console.log(x)
-//     switch (newPlayer) {
-//         case 'X_CLASS':
-//             player1 = newPlayer;
-//             x.classList.add(newPlayer)
-//             console.log(player1)
-//             break;
-//         case 'O_CLASS':
-//             currentPlayer = 'circle'
-//             o.classList.add(currentPlayer)
-//             player1 = currentPlayer;
-//     }
 
 
-// }
+// choose mark function
+const toggleSelection = () => {
+    const o = document.getElementById('o-mark-style')
+    const x = document.getElementById('x-mark-style')
+    // player1 = o.className
+
+    o.addEventListener('click', () => {
+        p1 = 'o'
+        p2 = 'x'
+        player1.innerHTML = 'X (P2)'
+        player2.innerHTML = 'O (P1)'
+    })
+
+    x.addEventListener('click', () => {
+        p1 = 'x'
+        p2 = 'o'
+        player1.innerHTML = 'X (P1)'
+        player2.innerHTML = 'O (P2)'
+    })
+
+}
+const toggleSelectionVsCPU = (player) => {
+    const o = document.getElementById('o-mark-style')
+    const x = document.getElementById('x-mark-style')
+    player = o.className
+
+    o.addEventListener('click', () => {
+        player = 'o'
+        p2 = 'x'
+        player1.innerHTML = 'X (CPU)'
+        player2.innerHTML = 'O (P1)'
+    })
+
+    x.addEventListener('click', () => {
+        player = 'x'
+        p2 = 'o'
+        player1.innerHTML = 'X (P1)'
+        player2.innerHTML = 'O (CPU)'
+    })
+
+}
 function endGame(draw) {
 
 
 }
 
+// function reset() {
+//     const restart = document.getElementById('restart')
+//     restart.addEventListener('click', () => {
+//         game
+//     })
 
-function reset() {
-    const restart = document.getElementById('restart')
-    restart.addEventListener('click', () => {
-        game
-    })
-
-}
+// }
 
 
 function main() {
-    // toggleSelection()
+    toggleSelection()
+    toggleSelectionVsCPU()
     getBoard()
     getVersus()
     reset()
